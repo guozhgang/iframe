9b843caf73c5beaa10e41e63d9ce2671b5b25e53
guozhgang <guozhgang@qq.com>
2014-11-10 17:39:40 +0800

    22

--- src/com/skss/iframe/util/ApplicationContextUtils.java
+++ src/com/skss/iframe/util/ApplicationContextUtils.java
@@ -1,12 +1,17 @@
 package com.skss.iframe.util;
 
 
+import java.util.List;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.skss.app.entity.Menu;
+import com.skss.app.service.MenuService;
 
 public class ApplicationContextUtils  implements ServletContextListener{
 	private ServletContext application;
@@ -23,6 +28,9 @@ public class ApplicationContextUtils  implements ServletContextListener{
 		application = arg0.getServletContext();
 		String path = application.getContextPath();
 		application.setAttribute("path", path);
+		MenuService menuService = (MenuService)applicationContext.getBean("menuService");
+		List<Menu> list = menuService.findMenuByAsync(null);
+		System.out.println(list.size());
 	}
 	
 	public static Object getBean(String name) {
